ab041f68bb1a6fa3c58e5d0a289b155194fa878f
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-23 17:49:45 +0300

    eteenpäin

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
-const cors = require('cors')
+const Note = require('./models/note')
 
-app.use(cors())
+const app = express()
+
 app.use(express.json())
 app.use(express.static('dist'))
 
@@ -29,18 +30,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -61,23 +59,20 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
